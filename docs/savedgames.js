1fbfdd29c52477f814497ab173bb45cce758eceb
YYBartT <bteunis@opera.com>
2025-08-19 23:16:41 +0200

    docs(general): Demo code for handling conflicts is incorrect YoYoGames/GMEXT-GameCenter#12
    Added missing members to "GameCenter_SavedGames_Save" and "GameCenter_SavedGames_ResolveConflict" async events

--- docs/savedgames.js
+++ docs/savedgames.js
@@ -20,7 +20,7 @@
 
 /**
  * @func GameCenter_SavedGames_Save
- * @desc This function requests the Apple GameCenter API to save a data string to a save slot. If the slot doesn't exist it gets created (if it exists data is overwritten). The function will not return any value but it will create a request that will trigger a ${event.social} callback when the task is resolved.
+ * @desc This function requests the Apple GameCenter API to save a data string to a save slot. If the slot doesn't exist it gets created, if it exists data is overwritten. The function will not return any value but it will create a request that will trigger a ${event.social} callback when the task is resolved.
  * 
  * @param {string} name The unique identifier of the save game slot.
  * @param {string} data The data string to be saved. Note that you can save complex data by using structs/arrays/maps/lists and converting them to strings.
@@ -28,9 +28,11 @@
  * @event social
  * @desc Triggered on save.
  * @member {string} type The string `"GameCenter_SavedGames_Save"`
- * @member {boolean} success Whether or not the task succeeded.
  * @member {real} name The unique identifier of the save game slot.
- * @member {string} slots A **JSON formatted string** following the ${struct.SlotJSON} structure. (can be parsed into a struct using ${function.json_parse})
+ * @member {string} slot A **JSON formatted string** following the ${struct.SlotJSON} structure. (can be parsed into a struct using ${function.json_parse})
+ * @member {boolean} success Whether or not the task succeeded.
+ * @member {real} [error_code] The error code, in case of an error
+ * @member {real} [error_message] The error message, in case of an error
  * @event_end
  * 
  * @event social
@@ -95,13 +97,17 @@
 
 /**
  * @func GameCenter_SavedGames_ResolveConflict
- * @desc This function requests the Apple GameCenter API to perform a conflict resolution given a conflict id. The function will not return any value but it will create a request that will trigger a ${event.social} callback when the task is resolved.
- * @param {real} conflict_id The unique identifier of the conflict.
+ * @desc This function requests the Apple GameCenter API to perform a conflict resolution given a conflict ID. The function will not return any value but it will create a request that will trigger a ${event.social} callback when the task is resolved.
+ * 
+ * @param {real} conflict_id The unique identifier of the conflict
  * @param {string} data A string containing the data you want to save (note that you can use a JSON formatted string to store data)
+ * 
  * @event social
  * @member {string} type The string `"GameCenter_SavedGames_ResolveConflict"`
- * @member {boolean} success Whether or not the task succeeded.
- * @member {real} conflictIndex The unique identification index of the resolved conflict.
+ * @member {real} conflict_id The unique identification index of the resolved conflict
+ * @member {boolean} success Whether or not the task succeeded
+ * @member {real} [error_code] The error code, in case of an error
+ * @member {real} [error_message] The error message, in case of an error
  * @event_end
  * 
  * @example
